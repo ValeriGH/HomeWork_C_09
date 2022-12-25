471e92c6081a028566e4ff4df17ac361bed6b04e
Valeria <valeria_korzun@mail.ru>
2022-12-25 23:09:02 +0300

    Сумма из промежутка чисел

---
+++ Task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumRec(int m, int n)
+{
+    if (m == n)
+        return m;
+    else
+        return SumRec(m + 1, n) + m;
+}
+
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма от {m} до {n}: {SumRec(m, n)}");
